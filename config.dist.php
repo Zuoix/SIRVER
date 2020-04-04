1d34d3bd0ea3a9b62400e7a43be9ff8e6747d2f3
Zuo Bruno <zuobruno@gmail.com>
2020-04-04 11:51:39 +0100

    Update config.dist.php

--- config.dist.php
+++ config.dist.php
@@ -12,5 +12,5 @@ define('AUTH_KEY', 'Enter Your Auth Key Here');
  * Server Url
  *
  */
-define('SERVER', 'https://sirver.me/admin/index.php?auth_key='.AUTH_KEY.'&route=api');
+define('SERVER', 'https://sirver.me/app/admin/index.php?auth_key='.AUTH_KEY.'&route=api');
 
